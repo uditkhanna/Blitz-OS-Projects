9e2e67c0e03b52469cfd0db9654ccc39f1c03557
Udit Khanna <ukhanna@ada.cs.pdx.edu>
2017-11-02 22:09:06 -0700

    p3 created

---
+++ p3/Main.s
@@ -0,0 +1,264 @@
+! Name of package being compiled: Main
+! 
+! Symbols from runtime.s
+	.import	_putString
+	.import	_heapInitialize
+	.import	_heapAlloc
+	.import	_heapFree
+	.import	_IsKindOf
+	.import	_RestoreCatchStack
+	.import	_PerformThrow
+	.import	_runtimeErrorOverflow
+	.import	_runtimeErrorZeroDivide
+	.import	_runtimeErrorNullPointer
+	.import	_runtimeErrorUninitializedObject
+	.import	_runtimeErrorWrongObject
+	.import	_runtimeErrorWrongObject2
+	.import	_runtimeErrorWrongObject3
+	.import	_runtimeErrorBadObjectSize
+	.import	_runtimeErrorDifferentArraySizes
+	.import	_runtimeErrorWrongArraySize
+	.import	_runtimeErrorUninitializedArray
+	.import	_runtimeErrorBadArrayIndex
+	.import	_runtimeErrorNullPointerDuringCall
+	.import	_runtimeErrorArrayCountNotPositive
+	.import	_runtimeErrorRestoreCatchStackError
+	.text
+! ErrorDecls
+	.import	_Error_P_System_UncaughtThrowError
+	.align
+! Functions imported from other packages
+	.import	print
+	.import	printInt
+	.import	printHex
+	.import	printChar
+	.import	printBool
+	.import	_P_System_MemoryEqual
+	.import	_P_System_StrEqual
+	.import	_P_System_StrCopy
+	.import	_P_System_StrCmp
+	.import	_P_System_Min
+	.import	_P_System_Max
+	.import	_P_System_printIntVar
+	.import	_P_System_printHexVar
+	.import	_P_System_printBoolVar
+	.import	_P_System_printCharVar
+	.import	_P_System_printPtr
+	.import	_P_System_nl
+	.import	Cleari
+	.import	Seti
+	.import	Wait
+	.import	RuntimeExit
+	.import	getCatchStack
+	.import	MemoryZero
+	.import	MemoryCopy
+	.import	_P_System_KPLSystemInitialize
+	.import	_P_System_KPLMemoryAlloc
+	.import	_P_System_KPLMemoryFree
+	.import	_P_System_KPLUncaughtThrow
+	.import	_P_System_KPLIsKindOf
+	.import	_P_System_KPLSystemError
+	.import	_P_Thread_InitializeScheduler
+	.import	_P_Thread_Run
+	.import	_P_Thread_PrintReadyList
+	.import	_P_Thread_ThreadStartMain
+	.import	_P_Thread_ThreadFinish
+	.import	_P_Thread_FatalError
+	.import	_P_Thread_SetInterruptsTo
+	.import	_P_Thread_TimerInterruptHandler
+	.import	Switch
+	.import	ThreadStartUp
+! Externally visible functions in this package
+	.export	_mainEntry
+	.export	main
+! The following class and its methods are from other packages
+	.import	_P_System_Object
+! The following class and its methods are from other packages
+	.import	_P_List_List
+! The following class and its methods are from other packages
+	.import	_P_List_Listable
+! The following class and its methods are from other packages
+	.import	_P_Thread_Thread
+! The following class and its methods are from other packages
+	.import	_P_Synch_Semaphore
+! The following class and its methods are from other packages
+	.import	_P_Synch_Mutex
+! The following class and its methods are from other packages
+	.import	_P_Synch_Mutex2
+! The following class and its methods are from other packages
+	.import	_P_Synch_Condition
+! The following interfaces are from other packages
+! The following interfaces are from this package
+! Globals imported from other packages
+	.import	_P_Thread_readyList
+	.import	_P_Thread_currentThread
+	.import	_P_Thread_mainThread
+	.import	_P_Thread_idleThread
+	.import	_P_Thread_threadsToBeDestroyed
+	.import	_P_Thread_currentInterruptStatus
+! Global variables in this package
+	.data
+	.align
+! String constants
+_StringConst_1:
+	.word	17			! length
+	.ascii	"Need to implement"
+	.align
+	.text
+! 
+! =====  MAIN ENTRY POINT  =====
+! 
+_mainEntry:
+	set	_packageName,r2		! Get CheckVersion started
+	set	0x87d0aa21,r3		! .  hashVal = -2016368095
+	call	_CheckVersion_P_Main_	! .
+	cmp	r1,0			! .
+	be	_Label_2		! .
+	ret				! .
+_Label_2:				! .
+	call	_heapInitialize
+	jmp	main
+! 
+! Source Filename and Package Name
+! 
+_sourceFileName:
+	.ascii	"Main.c\0"
+_packageName:
+	.ascii	"Main\0"
+	.align
+!
+! CheckVersion
+!
+!     This routine is passed:
+!       r2 = ptr to the name of the 'using' package
+!       r3 = the expected hashVal for 'used' package (myPackage)
+!     It prints an error message if the expected hashVal is not correct
+!     It then checks all the packages that 'myPackage' uses.
+!
+!     This routine returns:
+!       r1:  0=No problems, 1=Problems
+!
+!     Registers modified: r1-r4
+!
+_CheckVersion_P_Main_:
+	.export	_CheckVersion_P_Main_
+	set	0x87d0aa21,r4		! myHashVal = -2016368095
+	cmp	r3,r4
+	be	_Label_3
+	set	_CVMess1,r1
+	call	_putString
+	mov	r2,r1			! print using package
+	call	_putString
+	set	_CVMess2,r1
+	call	_putString
+	set	_packageName,r1		! print myPackage
+	call	_putString
+	set	_CVMess3,r1
+	call	_putString
+	set	_packageName,r1		! print myPackage
+	call	_putString
+	set	_CVMess4,r1
+	call	_putString
+	mov	r2,r1			! print using package
+	call	_putString
+	set	_CVMess5,r1
+	call	_putString
+	set	_packageName,r1		! print myPackage
+	call	_putString
+	set	_CVMess6,r1
+	call	_putString
+	mov	1,r1
+	ret	
+_Label_3:
+	mov	0,r1
+! Make sure _P_System_ has hash value 0xfe42cccc (decimal -29176628)
+	set	_packageName,r2
+	set	0xfe42cccc,r3
+	call	_CheckVersion_P_System_
+	.import	_CheckVersion_P_System_
+	cmp	r1,0
+	bne	_Label_4
+! Make sure _P_List_ has hash value 0xafebcabb (decimal -1343501637)
+	set	_packageName,r2
+	set	0xafebcabb,r3
+	call	_CheckVersion_P_List_
+	.import	_CheckVersion_P_List_
+	cmp	r1,0
+	bne	_Label_4
+! Make sure _P_Thread_ has hash value 0xd98e6cc5 (decimal -644977467)
+	set	_packageName,r2
+	set	0xd98e6cc5,r3
+	call	_CheckVersion_P_Thread_
+	.import	_CheckVersion_P_Thread_
+	cmp	r1,0
+	bne	_Label_4
+! Make sure _P_Synch_ has hash value 0x8e356f6d (decimal -1909100691)
+	set	_packageName,r2
+	set	0x8e356f6d,r3
+	call	_CheckVersion_P_Synch_
+	.import	_CheckVersion_P_Synch_
+	cmp	r1,0
+	bne	_Label_4
+_Label_4:
+	ret
+_CVMess1:	.ascii	"\nVERSION CONSISTENCY ERROR: Package '\0"
+_CVMess2:	.ascii	"' uses package '\0"
+_CVMess3:	.ascii	"'.  Whenever a header file is modified, all packages that use that package (directly or indirectly) must be recompiled.  The header file for '\0"
+_CVMess4:	.ascii	"' has been changed since '\0"
+_CVMess5:	.ascii	"' was compiled last.  Please recompile all packages that depend on '\0"
+_CVMess6:	.ascii	"'.\n\n\0"
+	.align
+! 
+! ===============  FUNCTION main  ===============
+! 
+main:
+	push	r14
+	mov	r15,r14
+	push	r13
+	set	_RoutineDescriptor_main,r1
+	push	r1
+	mov	2,r1
+_Label_8:
+	push	r0
+	sub	r1,1,r1
+	bne	_Label_8
+	mov	10,r13		! source line 10
+	mov	"\0\0FU",r10
+! VARIABLE INITIALIZATION...
+! CALL STATEMENT...
+!   _temp_5 = _StringConst_1
+	set	_StringConst_1,r1
+	store	r1,[r14+-12]
+!   Prepare Argument: offset=8  value=_temp_5  sizeInBytes=4
+	load	[r14+-12],r1
+	store	r1,[r15+0]
+!   Call the function
+	mov	11,r13		! source line 11
+	mov	"\0\0CA",r10
+	call	_P_Thread_FatalError
+! RETURN STATEMENT...
+	mov	11,r13		! source line 11
+	mov	"\0\0RE",r10
+	add	r15,12,r15
+	pop	r13
+	pop	r14
+	ret
+! 
+! Routine Descriptor
+! 
+_RoutineDescriptor_main:
+	.word	_sourceFileName
+	.word	_Label_6
+	.word	0		! total size of parameters
+	.word	8		! frame size = 8
+	.word	_Label_7
+	.word	-12
+	.word	4
+	.word	0
+_Label_6:
+	.ascii	"main\0"
+	.align
+_Label_7:
+	.byte	'?'
+	.ascii	"_temp_5\0"
+	.align
